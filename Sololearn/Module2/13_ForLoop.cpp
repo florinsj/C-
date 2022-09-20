9fbf456cc728dd02a6620c26efa6bc7faa0b01a4
Florin Stanciu <florinspad@gmail.com>
2022-09-20 14:24:40 +0300

    SoloLearn C++ - For loop

---
+++ Sololearn/Module2/13_ForLoop.cpp
@@ -0,0 +1,38 @@
+// for loop
+// is a repetition control loop that allows you to run a set of statements, a specific number of times
+// for(init;condition;increment) { statementsSet}
+// the loop's body repeat itself until the condition become false;
+// for (int i=1;i<5;i++) {//some code}
+// the init and increment can be left out , but semicolons are mandatory !!
+// int i=1; for(;i<10;) {  statements; i++; etc;}
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    for (int a = 0; a < 10; a++)
+    {
+        cout << a << endl;
+    }
+
+    int i = 10;
+    for (; i < 20;) // here we left out the init and increment
+    {
+        cout << i << endl;
+        i++;
+    }
+
+    for (int a = 0; a < 10; a += 2)
+    {
+        cout << a << endl;
+    }
+
+    for (int a = 100; a >= 80; a -= 2)
+    {
+        cout << a << endl;
+    }
+
+    return 0;
+}
---
+++ Sololearn/Module2/14_ForLoopPractice.cpp
@@ -0,0 +1,18 @@
+// You are on a 5 hour sea voyage. The ship sails at a speed of 40 km per hour.
+// Write a program that will output how many kilometers the ship has traveled by each hour.
+// ​Remember to use endl to create a new line for every iteration output.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int distance = 0;
+    // your code goes here
+    for (int h = 1; h <= 5; h++)
+    {
+        cout << h * 40 << endl;
+    }
+
+    return 0;
+}
