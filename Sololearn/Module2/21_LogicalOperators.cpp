affccf699121e0d319fc5c50cea747887b7610a1
Florin Stanciu <florinspad@gmail.com>
2022-09-20 20:07:20 +0300

    SoloLearn C++ - Logical operators

---
+++ Sololearn/Module2/21_LogicalOperators.cpp
@@ -0,0 +1,41 @@
+// logical operators
+// logical operators are used to combine conditional statements
+// &&   AND --> true if each condition from entire expression is true
+// ||   OR  --> true if at least one condition from entire expression is true
+// !    NOT --> single operand, reversing logical state -->!false=true
+// all the logical operators can be combined
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // AND
+    int age = 20;
+    int grade = 80;
+    if (age > 16 && age < 60)
+    {
+        cout << "Accepted" << endl;
+    }
+    if (age > 16 && age < 60 && grade > 50)
+    {
+        cout << "Accepted" << endl;
+    }
+
+    // OR
+    age = 16;
+    int score = 90;
+    if (age > 20 || score > 50)
+    {
+        cout << "Accepted!" << endl;
+    }
+
+    // NOT
+    age = 10;
+    if (!(age > 16))
+    {
+        cout << "Your age is less than 16" << endl;
+    }
+
+    return 0;
+}
---
+++ Sololearn/Module2/22_LogicalOpPractice.cpp
@@ -0,0 +1,26 @@
+// Logical operators
+
+// Along with other physical health requirements,
+// astronaut candidates must have perfect vision(100 %) and their height must be between 62 and 75 inches.
+// Write a program that accordingly takes the vision percentage and height(in inches) as an input
+// and prints "passed" if given conditions are satisfied and "failed" if otherwise.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int vision;
+    cin >> vision;
+
+    int height;
+    cin >> height;
+    // your code goes here
+    if (vision == 100 && height > 62 && height < 75)
+        cout << "passed";
+    else
+        cout << "failed";
+
+    return 0;
+}
