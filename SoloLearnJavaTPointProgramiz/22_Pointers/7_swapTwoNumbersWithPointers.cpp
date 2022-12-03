b4fdb5dd221b5651060b6eb8271b9f8ed429230f
Florin Stanciu <florinspad@gmail.com>
2022-12-03 20:15:08 +0200

    C++ Pointers - swap two numbers without 3rd variable

---
+++ SoloLearnJavaTPointProgramiz/22_Pointers/7_swapTwoNumbersWithPointers.cpp
@@ -0,0 +1,17 @@
+// swap two numbers using pointers (without using 3rd variable) --    JavaTPoint
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int a = 20, b = 10;
+    int *p1 = &a;
+    int *p2 = &b;
+    cout << "Before swap : *p1= " << *p1 << " *p2= " << *p2 << endl;
+    *p1 = *p1 + *p2;
+    *p2 = *p1 - *p2;
+    *p1 = *p1 - *p2;
+    cout << "After swap : *p1= " << *p1 << " *p2= " << *p2 << endl;
+
+    return 0;
+}
