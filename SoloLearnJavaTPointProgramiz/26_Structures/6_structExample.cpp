e83a07efd568196ccadf49f51b322fa19008bf1d
Florin Stanciu <florinspad@gmail.com>
2022-12-30 13:03:36 +0200

    C++ struct

---
+++ SoloLearnJavaTPointProgramiz/26_Structures/6_structExample.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+struct product
+{
+    int weight;
+    double price;
+} apple, banana, melon; // declaration of struct and  variables of type struct
+int main()
+{
+
+    // main code
+    banana.weight = 2;
+    melon.weight = 6;
+
+    return 0;
+}
