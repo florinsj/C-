da8cd4b0f04fab407d47babbbd879a612416f7a0
Florin Stanciu <florinspad@gmail.com>
2022-12-30 12:12:16 +0200

    C++ cin.get()

---
+++ SoloLearnJavaTPointProgramiz/6_Basic IO/7_cinGet.cpp
@@ -0,0 +1,21 @@
+/*
+
+cin.get()   = used to acces char array.It includes whte spaces characters
+            = read a string with the whitespace
+
+syntax : cin.get(string_name,size);
+
+            */
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    char name[25];
+    cout << "Enter name:" << endl;
+    cin.get(name, 25);
+    cout << name;
+
+    return 0;
+}
