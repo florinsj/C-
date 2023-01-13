9c4454620fd2f6605ea69dd6c0b0be9b30e6ee10
Florin Stanciu <florinspad@gmail.com>
2023-01-13 15:13:17 +0200

    C++ - Enums

--- SoloLearnJavaTPointProgramiz/27_Enum/1_enum.cpp
+++ SoloLearnJavaTPointProgramiz/27_Enum/1_enum.cpp
@@ -1,7 +1,7 @@
 // JavaTPoint
 
 /*
-- enum = datatype that contains fixe set of constants
+- enum = datatype that contains fix set of constants
 - enum constansts = static and final implicitly
 - enum can be thought of as classes that have fixed set of constants
 
@@ -12,4 +12,32 @@ Enums
     - can be used in switch
     - can have fields, methds, constructors
 
+    -can implement many interfaces but cannot extend any class because it intern extend Enum class
+
+
 */
+
+// Exmaple Enum
+
+#include <iostream>
+using namespace std;
+enum week
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+};
+int main()
+{
+    week day;
+    day = Friday;
+
+    cout << day << endl;
+    cout << day + 1 << endl;
+
+    return 0;
+}
